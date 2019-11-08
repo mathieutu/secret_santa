ac52d04a2d061c4c13063ab77a4be9a49bf036e0
Mathieu TUDISCO <oss@mathieutu.dev>
2019-11-08 09:48:51 +0100

    📝 Change confirmation text.

--- resources/views/confirmation.blade.php
+++ resources/views/confirmation.blade.php
@@ -5,13 +5,14 @@
         <p class="mt-4">
             @if(!$alreadyKnown)
                 Merci de ton inscription jeune {{ $user->name }}&nbsp;! <br>
-                Tu es inscrit·e en tant que Secret Santa sur la contrib de <strong>{{ $user->city }}</strong>.<br>
+                Tu es inscrit·e en tant que Secret Santa sur la soirée de Noël de <strong>{{ $user->city }}</strong>.<br>
                 Tu recevras très prochainement un courriel t'indiquant les prochaines étapes&nbsp;! <br>
+                S'il y a des changements te concernant, n'hésite pas à tenir au courant la team HOM&nbsp;! 😉 <br>
             @else
                 Bonjour {{ $user->name }}&nbsp;! <br>
-                Je te reconnais, tu t'es déjà inscrit·e en tant que Secret Santa sur la contrib de
+                Je te reconnais, tu t'es déjà inscrit·e en tant que Secret Santa sur la soirée de Noël de
                 <strong>{{ $user->city }}</strong> {{ $user->created_at->diffForHumans() }}&nbsp;!<br>
-                S'il y a des changements te concernant, n'hésite pas à tenir au courant qui tu sais&nbsp;! 😉
+                S'il y a des changements te concernant, n'hésite pas à tenir au courant la team HOM&nbsp;! 😉
             @endif
         </p>
         <p class="mt-4">A très vite&nbsp;!</p>
